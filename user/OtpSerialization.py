981504b972883af4cb67ba38f0a330b732c6c0bf
nmadharia <92153486+nmadharia@users.noreply.github.com>
2021-10-22 19:21:27 +0530

    added login, signup, otp generate, forget pass, verify otp done. only otp storage pending.

---
+++ user/OtpSerialization.py
@@ -0,0 +1,10 @@
+from user.models import users_new
+from rest_framework import serializers
+
+class OtpserializerClass(serializers.ModelSerializer):
+   # acc_created = serializers.CharField(required=False)
+    #userId = serializers.CharField(required=False)
+
+    class Meta:
+        model= users_new
+        fields= list(["otp"])
